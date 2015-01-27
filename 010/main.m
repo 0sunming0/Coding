33315e861fcb5e2817a7989296b5e9670dedb479
student <student@studentdeMac-mini-80.local>
2015-01-27 14:02:02 +0800

    Initial Commit

---
+++ 010/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  010
+//
+//  Created by student on 15-1-27.
+//  Copyright (c) 2015年 student. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
